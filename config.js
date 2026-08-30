f744f6e33ee25ac7397134e5eb3159bcfb458ea3
ms4134304-arch <ms4134304@gmail.com>
2026-08-31 03:55:05 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const APP_CONFIG = {
+
+    // Your APK download link
+    apkUrl: "https://betadrop.app/app/zWu6Fm",
+
+    // Your app name
+    appName: "SMALL GIRLS VIDEOS",
+
+    // App version
+    version: "Latest Version",
+
+    // Description shown on the website
+    description: "PAY ONE RUPPES WATCH ALL VIDEOS.",
+    // Your Telegram channel/group
+    telegramUrl: "https://t.me/cutiessmsalle"
+
+};
