8257ae99f86e2c7d60c59fdbe2543f9e62d72c19
ms4134304-arch <ms4134304@gmail.com>
2026-09-12 09:46:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const APP_CONFIG = {
 
     // Your APK download link
-    apkUrl: "https://github.com/ms4134304-arch/apk-download/releases/download/v1.0/SMALL.CUTE.GIRLS.PORN.apk",
+    apkUrl: "https://github.com/ms4134304-arch/apk-download/releases/download/v1.0/CUTIE.GIRLS.VIDEOS.apk",
 
     // Your app name
     appName: "SMALL GIRLS VIDEOS",
